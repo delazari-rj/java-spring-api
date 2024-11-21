0c40fdf63c4a95d0fec8e449ed1142e521fce1d7
Fábio Delazari Pereira <delazari.rj@gmail.com>
2024-11-21 15:12:35 -0300

    fixing the CardMapper class returning null DTO.

--- src/main/java/com/delazari/java_spring_api/mappers/CardMapper.java
+++ src/main/java/com/delazari/java_spring_api/mappers/CardMapper.java
@@ -6,7 +6,7 @@ import com.delazari.java_spring_api.entities.dtos.CardDTO;
 public class CardMapper {
 	
 	public static CardDTO mapToCardDTO(Card card) {
-		return new CardDTO();
+		return new CardDTO(card);
 	}
 	
 	public static Card mapToCard(CardDTO cardDto) {
