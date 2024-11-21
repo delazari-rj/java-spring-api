86790b6a0cabd9c445719e46827e800abc82be61
Fábio Delazari Pereira <delazari.rj@gmail.com>
2024-11-21 11:21:44 -0300

    creating the project.

---
+++ src/test/java/com/delazari/java_spring_api/JavaSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delazari.java_spring_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
