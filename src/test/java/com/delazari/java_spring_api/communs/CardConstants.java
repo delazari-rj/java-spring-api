2fa16da1be7da3e56b6188865296e5231d071ac1
Fábio Delazari Pereira <delazari.rj@gmail.com>
2024-11-22 17:20:09 -0300

    adding delete unit tests to the CardRepository class and refectoring CardConstants class.

--- src/test/java/com/delazari/java_spring_api/communs/CardConstants.java
+++ src/test/java/com/delazari/java_spring_api/communs/CardConstants.java
@@ -4,11 +4,19 @@ import com.delazari.java_spring_api.entities.Card;
 
 public class CardConstants {
 
+	private static String longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum quis ante eu tempus. Morbi vitae porta sem, nec tristique elit. Maecenas semper aliquet risus, eget maximus libero vestibulum sed. Phasellus ut dignissim nisl, non consectetur tortor. Donec id tempor ligula. Suspendisse eleifend velit sit amet dignissim varius. Donec consequat neque eu velit blandit imperdiet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse varius neque quam. Nunc vestibulum diam vitae eros varius, non maximus massa tempus.\r\n"
+			+ "\r\n"
+			+ "Fusce ac rhoncus mi, eu auctor nunc. Aenean feugiat sit amet lectus at volutpat. Praesent rhoncus lorem sed urna tincidunt, quis dictum nulla suscipit. Donec eu velit molestie, porta nunc nec, dignissim mauris. Ut ac interdum tellus. Integer aliquam vitae risus eu malesuada. Mauris venenatis mi quis neque condimentum dignissim. In faucibus velit a quam malesuada, vel efficitur leo fermentum. Donec augue nunc, consequat ut est vitae, luctus tincidunt lectus. Nam vitae turpis in nibh sollicitudin molestie et in lacus. Morbi dignissim malesuada mattis. Cras placerat tempus leo, faucibus condimentum diam sollicitudin non. Pellentesque odio diam, ornare vel ex at, maximus cursus mauris. Cras arcu nisi, imperdiet vitae risus et, ultricies ullamcorper purus. Integer vel tortor sagittis, vestibulum lectus eget, placerat tortor. Aenean efficitur vitae ipsum ac aliquet.";
+	
+	private static String description = "Welcome to Magic: The Gathering! Magic™ is a trading card game where players summon incredible creatures and cast powerful spells to defeat their foes. The game has thousands of cards so you can always find a way to express yourself on the battlefield.";
+	
+	private static String name = "Birds of Paradise";
+	
 	public static final Card VALID_CARD_ID_NULL
-			= new Card(null, "Birds of Paradise", "Welcome to Magic: The Gathering! Magic™ is a trading card game where players summon incredible creatures and cast powerful spells to defeat their foes. The game has thousands of cards so you can always find a way to express yourself on the battlefield.");
+			= new Card(null, name, description);
 	
 	public static final Card VALID_CARD_ID_1L
-			= new Card(1L, "Birds of Paradise", "Welcome to Magic: The Gathering! Magic™ is a trading card game where players summon incredible creatures and cast powerful spells to defeat their foes. The game has thousands of cards so you can always find a way to express yourself on the battlefield.");
+			= new Card(1L, name, description);
 	
 	public static final Card VALID_CARD_UPDATED 
 			= new Card(1L, "Birds of Paradise UPDATED", "Welcome to Magic: The Gathering! UPDATED...");
@@ -17,5 +25,9 @@ public class CardConstants {
 			= new Card();
 	
 	public static final Card INVALID_CARD_EMPTY_NAME_FIELD 
-			= new Card(null, "", "Welcome to Magic: The Gathering! Magic™ is a trading card game where players summon incredible creatures and cast powerful spells to defeat their foes. The game has thousands of cards so you can always find a way to express yourself on the battlefield.");
+			= new Card(null, "", description);
+	
+	public static final Card INVALID_CARD_LONG_NAME_FIELD 
+			= new Card(null, longText, description);
+ 
 }
--- src/test/java/com/delazari/java_spring_api/repositories/CardRepositoryTest.java
+++ src/test/java/com/delazari/java_spring_api/repositories/CardRepositoryTest.java
@@ -2,8 +2,11 @@ package com.delazari.java_spring_api.repositories;
 
 import static com.delazari.java_spring_api.communs.CardConstants.INVALID_CARD_EMPTY_NAME_FIELD;
 import static com.delazari.java_spring_api.communs.CardConstants.INVALID_CARD_NULL;
+import static com.delazari.java_spring_api.communs.CardConstants.INVALID_CARD_LONG_NAME_FIELD;
 import static com.delazari.java_spring_api.communs.CardConstants.VALID_CARD_ID_NULL;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.NoSuchElementException;
@@ -33,6 +36,8 @@ public class CardRepositoryTest {
 	public void init() {
 		VALID_CARD_ID_NULL.setId(null);
 	}
+	
+	//LIST OF CREATION TESTS.
 	
 	@Test
 	public void create_WithValidData_ReturnCard() {
@@ -65,7 +70,14 @@ public class CardRepositoryTest {
 	}
 	
 	@Test
-	public void findById_WithValidIID_ReturnCard() {
+	public void create_WithTheLongName_ReturnDataIntegrityViolationException() {
+		assertThatThrownBy(() -> cardRepository.save(INVALID_CARD_LONG_NAME_FIELD)).isInstanceOf(DataIntegrityViolationException.class);
+	}
+	
+	//LIST OF FIND TESTS
+	
+	@Test
+	public void findById_WithValidID_ReturnCard() {
 		Card card = testEntityManager.persistAndFlush(VALID_CARD_ID_NULL);
 		
 		Card sut = cardRepository.findById(card.getId()).get();
@@ -85,5 +97,21 @@ public class CardRepositoryTest {
 		testEntityManager.persistAndFlush(VALID_CARD_ID_NULL);
 		
 		assertThatThrownBy(() -> cardRepository.findById(null).get()).isInstanceOf(InvalidDataAccessApiUsageException.class);
+	}
+	
+	//LIST OF DELETE TESTS
+	
+	@Test
+	public void deleteById_WithValidID_ReturnVoid() {
+		testEntityManager.persistAndFlush(VALID_CARD_ID_NULL);
+		
+		assertThatCode(() -> cardRepository.deleteById(1L)).doesNotThrowAnyException();
+	}
+	
+	@Test
+	public void deleteById_WithNullId_ReturnInvalidDataAccessApiUsageException() {
+		testEntityManager.persistAndFlush(VALID_CARD_ID_NULL);
+		
+		assertThatThrownBy(() -> cardRepository.deleteById(null)).isInstanceOf(InvalidDataAccessApiUsageException.class);
 	}
 }
